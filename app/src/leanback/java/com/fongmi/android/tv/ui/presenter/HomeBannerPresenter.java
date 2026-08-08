0948737b00fc8d1a470c309a08ecc0a95179080f
jiabihuan <jiabihuan@gmail.com>
2026-08-08 20:56:48 +0000

    fix: 补充缺失的 androidx.leanback.widget.Presenter import
    HomeBannerPresenter 继承 Leanback Presenter 但缺少 import 导致编译失败

--- app/src/leanback/java/com/fongmi/android/tv/ui/presenter/HomeBannerPresenter.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/presenter/HomeBannerPresenter.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.leanback.widget.Presenter;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
